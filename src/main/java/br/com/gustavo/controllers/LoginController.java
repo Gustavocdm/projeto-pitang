35ac2146b709ea77d62ae4e1335f237011bc3368
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-11-22 20:45:06 -0300

    controller de teste de conexão hibernate

---
+++ src/main/java/br/com/gustavo/controllers/LoginController.java
@@ -0,0 +1,83 @@
+package br.com.gustavo.controllers;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.gustavo.dominio.model.Telefone;
+import br.com.gustavo.dominio.model.Usuario;
+
+@Named
+@ViewScoped
+public class LoginController implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Usuario usuario;
+	private Telefone telefone;
+
+	@PostConstruct
+	public void Teste() {
+		System.out.println("Testando persistência");
+
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("projeto-pitang");
+		EntityManager em = emf.createEntityManager();
+		
+		Usuario usuario = new Usuario(); usuario.setNome("Gustavo");
+		usuario.setEmail("email_pessoal@hotmail.com");
+		usuario.setSenha("senha12345");
+		
+		em.getTransaction().begin();
+		System.out.println("Finalizou operacão");
+		
+		em.persist(usuario);
+		em.getTransaction().commit();		
+		
+		Telefone telefone1 = new Telefone();
+		Telefone telefone2 = new Telefone();
+		
+		this.usuario = em.find(Usuario.class, 1);
+		
+		telefone1.setDdd(81);
+		telefone1.setNumero("998450718");
+		telefone1.setTipo("celular");
+		telefone1.setUsuario(this.usuario);
+		
+		telefone2.setDdd(81);
+		telefone2.setNumero("32715463");
+		telefone2.setTipo("fixo");
+		telefone2.setUsuario(this.usuario);
+		
+		
+		em.getTransaction().begin();
+		
+		em.persist(telefone1);
+		em.persist(telefone2);
+		
+		
+		this.telefone= em.find(Telefone.class, 1);
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Telefone getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+
+}
