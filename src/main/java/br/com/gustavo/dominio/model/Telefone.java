1c0cf5b36c37a89b8265fced92774f89a3f6e1eb
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-11-22 18:21:06 -0300

    hibernate e h2

--- src/main/java/br/com/gustavo/dominio/model/Telefone.java
+++ src/main/java/br/com/gustavo/dominio/model/Telefone.java
@@ -1,11 +1,36 @@
 package br.com.gustavo.dominio.model;
 
-public class Telefone {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Telefone implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
 	private Integer ddd;
 	private String numero;
 	private String tipo;
 	
+	@ManyToOne
+	@JoinColumn(name = "usuario_id")
+	private Usuario usuario;
 	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public Integer getDdd() {
 		return ddd;
 	}
@@ -23,6 +48,41 @@ public class Telefone {
 	}
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Telefone other = (Telefone) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Telefone [id=" + id + ", ddd=" + ddd + ", numero=" + numero + ", tipo=" + tipo + ", usuario=" + usuario
+				+ "]";
 	}
 	
 }
--- src/main/java/br/com/gustavo/dominio/model/Usuario.java
+++ src/main/java/br/com/gustavo/dominio/model/Usuario.java
@@ -1,17 +1,39 @@
 package br.com.gustavo.dominio.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
-public class Usuario {
+
+@Entity
+public class Usuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="usuario_id")
+	private Integer id;
 	private String nome;
 	private String email;
 	private String senha;
 	
+	@OneToMany(mappedBy="usuario")
 	private List<Telefone> telefones = new ArrayList<>();
 	
-	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return nome;
 	}
@@ -34,7 +56,35 @@ public class Usuario {
 	public List<Telefone> getTelefones() {
 		return telefones;
 	}
-	public void addTelefone(Telefone telefone) {
-		telefones.add(telefone);
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", nome=" + nome + ", email=" + email + ", senha=" + senha + ", telefones="
+				+ telefones + "]";
+	}
+	
+	
 }
