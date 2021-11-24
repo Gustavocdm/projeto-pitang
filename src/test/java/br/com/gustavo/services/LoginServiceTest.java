ce0b191b6c34db5d4d481360bc75b264340fc13f
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-11-24 11:25:10 -0300

    test: garantindo que retorna false se CompararHash retornar false, chamando CompararHash e FindUserByEmail com valores corretos.

--- src/test/java/br/com/gustavo/services/LoginServiceTest.java
+++ src/test/java/br/com/gustavo/services/LoginServiceTest.java
@@ -1,7 +1,9 @@
 package br.com.gustavo.services;
 
+import static br.com.gustavo.builders.UsuarioBuilder.umUsuario;
 import static org.junit.Assert.assertFalse;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -10,6 +12,7 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import br.com.gustavo.dominio.model.Usuario;
 import br.com.gustavo.services.protocols.CompararHash;
 import br.com.gustavo.services.protocols.FindUserByEmail;
 
@@ -29,6 +32,15 @@ public class LoginServiceTest {
 	}
 	
 	@Test
+	public void deveChamarUsuarioRepositoryComEmailPassado() {
+		String email = "email";
+		
+		sut.logar(email, "senha");
+		
+		verify(usuarioRepository).findByEmail(email);
+	}
+	
+	@Test
 	public void deveRetornarFalseSeUsuarioNaoEncontrado() {
 		when(usuarioRepository.findByEmail(anyString())).thenReturn(null);
 		
@@ -36,4 +48,33 @@ public class LoginServiceTest {
 		
 		assertFalse(login);
 	}
+
+	
+	@Test
+	public void deveChamarCompararHashComEmailESenhaPassados() {
+		
+		Usuario usuario = umUsuario().comSenha("senha_entidade").pegar();
+		
+		String senha = "senha";
+		
+		when(usuarioRepository.findByEmail(anyString())).thenReturn(usuario);
+		
+		sut.logar("email", senha);
+		
+		verify(compararHash).compararHash(senha, usuario.getSenha());
+	}
+	
+	@Test
+	public void deveRetornarFalseSeSenhaNaoBaterComHash() {
+		Usuario usuario = umUsuario().pegar();
+		
+		when(usuarioRepository.findByEmail(anyString())).thenReturn(usuario);
+		
+		when(compararHash.compararHash(anyString(), anyString())).thenReturn(false);
+		
+		boolean login = sut.logar("email", "senha");
+		
+		assertFalse(login);
+	}
+	
 }
