91d5d307795c3078af65b5de21599e0724cd4eb9
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-11-24 21:12:28 -0300

    controller de cadastro de usuarios, garantindo que irá retornar string vazia se uma exceção ocorrer

--- src/main/java/br/com/gustavo/controllers/CadastroController.java
+++ src/main/java/br/com/gustavo/controllers/CadastroController.java
@@ -2,12 +2,56 @@ package br.com.gustavo.controllers;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.gustavo.dominio.model.Telefone;
+import br.com.gustavo.dominio.model.Usuario;
+import br.com.gustavo.dominio.usecases.CadastroUsuario;
 
 @Named
 @ViewScoped
 public class CadastroController implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Usuario usuario;
 	
+	@Inject
+	private CadastroUsuario cadastroUsuario;
+	
+	
+	@PostConstruct
+	public void setup() {
+		addTelefone();
+	}
+	
+	public String cadastrar() {
+		try {
+			cadastroUsuario.cadastrarUsuario(usuario);
+		}
+		catch(Exception e) {
+			return "";
+		}
+		
+		return "dawdwa";
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public void addTelefone() {
+		usuario.getTelefones().add(new Telefone());
+	}
+	
+	public void removeTelefone(int index) {
+		usuario.getTelefones().remove(index);
+	}
 }
