3f2c74910e0bdd93d0dd8c080bdbf47da0d299ad
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-11-25 20:44:48 -0300

    login sessão

---
+++ src/main/java/br/com/gustavo/controllers/sessao/LoginSessao.java
@@ -0,0 +1,28 @@
+package br.com.gustavo.controllers.sessao;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import br.com.gustavo.dominio.model.Usuario;
+
+@Named
+@SessionScoped
+public class LoginSessao implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Usuario usuarioLogado = null;
+	
+	public void logarUsuarioNaSessao(Usuario usuario) {
+		usuarioLogado = usuario;
+	}
+	
+	public void deslogarUsuarioDaSessao() {
+		usuarioLogado = null;
+	}
+
+	public Usuario getUsuarioLogado() {
+		return usuarioLogado;
+	}	
+}
