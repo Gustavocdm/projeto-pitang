db935a5c1a8812e5c7727c9648c54478d761a7da
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-11-25 00:40:15 -0300

    to do

---
+++ src/main/java/br/com/gustavo/controllers/utils/validation/CadastroUsuarioValidacao.java
@@ -0,0 +1,28 @@
+package br.com.gustavo.controllers.utils.validation;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+
+import br.com.gustavo.controllers.exceptions.InputInvalidoException;
+import br.com.gustavo.controllers.exceptions.TipoInputInvalidoException;
+import br.com.gustavo.dominio.model.Usuario;
+
+@RequestScoped
+public class CadastroUsuarioValidacao implements Validacao, Serializable {
+	private static final long serialVersionUID = 1L;
+		
+	@Override
+	public InputInvalidoException validar(Object input) throws TipoInputInvalidoException {
+		if (! (input instanceof Usuario)) {
+			throw new TipoInputInvalidoException("Input não é do tipo Usuario");
+		}
+		
+		Usuario usuario = (Usuario) input;
+		
+		
+		
+		return null;
+	}
+
+}
---
+++ src/test/java/br/com/gustavo/controllers/LoginControllerTest.java
@@ -0,0 +1,6 @@
+package br.com.gustavo.controllers;
+
+public class LoginControllerTest {
+	
+	
+}
