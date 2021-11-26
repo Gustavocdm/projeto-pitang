436faec8a2bcff15957d5e48cf9c7d37e0aef405
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-11-26 02:06:40 -0300

    atualizando e deletando usuário

--- src/main/java/br/com/gustavo/services/UsuarioService.java
+++ src/main/java/br/com/gustavo/services/UsuarioService.java
@@ -7,13 +7,15 @@ import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import br.com.gustavo.dominio.model.Usuario;
+import br.com.gustavo.dominio.usecases.AtualizarEntidade;
 import br.com.gustavo.dominio.usecases.CadastroUsuario;
+import br.com.gustavo.dominio.usecases.DeletarEntidade;
 import br.com.gustavo.dominio.usecases.ListarEntidades;
 import br.com.gustavo.services.protocols.Hasher;
 import br.com.gustavo.services.protocols.UsuarioRepository;
 
 @Dependent
-public class UsuarioService implements CadastroUsuario, ListarEntidades<Usuario>, Serializable {
+public class UsuarioService implements CadastroUsuario, ListarEntidades<Usuario>, DeletarEntidade<Usuario>, AtualizarEntidade<Usuario>, Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Inject
@@ -35,6 +37,25 @@ public class UsuarioService implements CadastroUsuario, ListarEntidades<Usuario>
 	@Override
 	public List<Usuario> listar() {
 		return usuarioRepository.list();
+	}
+
+	@Override
+	public void deletar(Usuario entidade) {
+		usuarioRepository.delete(entidade);
+	}
+
+	@Override
+	public Usuario atualizar(Usuario entity) {
+		if (entity.getSenha().length() > 0) {
+			String senhaHash = hasher.hash(entity.getSenha());
+			entity.setSenha(senhaHash);
+		}
+		else {
+			Usuario usuarioCadastrado = usuarioRepository.findById(entity.getId());
+			entity.setSenha(usuarioCadastrado.getSenha());
+		}
+		Usuario usuario = usuarioRepository.update(entity);
+		return usuario;
 	}
 	
 	
