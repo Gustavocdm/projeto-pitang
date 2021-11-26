baac67ce91896f1c6a32157e091f0da480e16a70
Gustavo <47989383+Gustavocdm@users.noreply.github.com>
2021-11-25 22:26:35 -0300

    test: LoginController e CadastroController

--- src/test/java/br/com/gustavo/controllers/CadastroControllerTest.java
+++ src/test/java/br/com/gustavo/controllers/CadastroControllerTest.java
@@ -1,7 +1,9 @@
 package br.com.gustavo.controllers;
 
+import static br.com.gustavo.builders.UsuarioBuilder.umUsuario;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -10,6 +12,8 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import br.com.gustavo.controllers.sessao.LoginSessao;
+import br.com.gustavo.dominio.model.Usuario;
 import br.com.gustavo.dominio.usecases.CadastroUsuario;
 
 public class CadastroControllerTest {
@@ -20,18 +24,44 @@ public class CadastroControllerTest {
 	@Mock
 	CadastroUsuario cadastroUsuario;
 	
+	@Mock
+	LoginSessao loginSessao;
+	
+	@Mock
+	Usuario usuarioSut;
+	
 	@Before
 	public void setup() {
 		initMocks(this);
 	}
 	
 	@Test
-	public void deveRetornarStringVaziaSeCadastroUsuarioLancarExcecao() {
+	public void deveRetornarUrlVaziaSeCadastroUsuarioLancarExcecao() {
 		when(cadastroUsuario.cadastrarUsuario(any())).thenThrow(new RuntimeException("any_message"));
 		
 		String url = sut.cadastrar();
 		
 		assertEquals("", url);		
+	}
+	
+	@Test
+	public void deveLogarNaSessaoComUsuarioCriado() {
+		Usuario usuario = umUsuario().comId(3).pegar();
+		
+		when(cadastroUsuario.cadastrarUsuario(any())).thenReturn(usuario);
+		
+		sut.cadastrar();
+		
+		verify(loginSessao).logarUsuarioNaSessao(usuario);
+	}
+	
+	@Test
+	public void deveRedirecionarParaOIndexSeUsuarioCadastradoComSucesso() {
+		when(cadastroUsuario.cadastrarUsuario(any())).thenReturn(new Usuario());
+		
+		String url = sut.cadastrar();
+		
+		assertEquals("/auth/index.xhtml?faces-redirect=true", url);
 	}
 	
 }
--- src/test/java/br/com/gustavo/controllers/LoginControllerTest.java
+++ src/test/java/br/com/gustavo/controllers/LoginControllerTest.java
@@ -1,6 +1,75 @@
 package br.com.gustavo.controllers;
 
+import static br.com.gustavo.builders.UsuarioBuilder.umUsuario;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import br.com.gustavo.controllers.sessao.LoginSessao;
+import br.com.gustavo.dominio.model.Usuario;
+import br.com.gustavo.dominio.usecases.Login;
+
 public class LoginControllerTest {
 	
+	@InjectMocks
+	LoginController sut;
 	
+	@Mock
+	Login login;
+	
+	@Mock
+	LoginSessao loginSessao;
+	
+	@Before
+	public void setup() {
+		initMocks(this);
+	}
+	
+	@Test
+	public void deveRetornarUrlVaziaSeLoginRetornaNull() {
+		when(login.logar(anyString(), anyString())).thenReturn(null);
+		
+		String url = sut.logar();
+		
+		assertEquals("", url);
+	}
+	
+	@Test
+	public void deveLogarNaSessaoSeLoginFoiBemSucedido() {
+		Usuario usuario = umUsuario().comId(3).pegar();
+		
+		when(login.logar(anyString(), anyString())).thenReturn(usuario);
+		
+		sut.logar();
+		
+		verify(loginSessao).logarUsuarioNaSessao(usuario);
+	}
+	
+	@Test
+	public void deveRetornarUrlDoIndexSeLoginEfetuadoComSucesso() {
+		Usuario usuario = umUsuario().pegar();
+		
+		when(login.logar(anyString(), anyString())).thenReturn(usuario);
+		
+		String url = sut.logar();
+		
+		
+		assertEquals("/auth/index.xhtml?faces-redirect=true", url);
+	}
+	
+	@Test
+	public void deveRetornarUrlVaziaSeLoginLancarExcecao() {
+		when(login.logar(anyString(), anyString())).thenThrow(new RuntimeException());
+		
+		String url = sut.logar();
+		
+		assertEquals("", url);
+	}
 }
